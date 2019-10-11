857a1eae4f3caf3d1435268c6559c70492713274
Antalex <aantalex@gmail.com>
2019-10-11 19:09:02 +0300

    dev

--- src/app/components/shared/table-quotes/table-quotes.component.ts
+++ src/app/components/shared/table-quotes/table-quotes.component.ts
@@ -9,7 +9,7 @@ import {QuotesService} from '../../../services/quotes.service';
 export class TableQuotesComponent implements OnInit {
   @Input() height = 300;
   dataSource = [];
-  displayedColumns: string[] = ['bid-open', 'bid-close', 'price', 'offer-open', 'offer-close'];
+  displayedColumns: string[] = ['bid-open', 'bid-close', 'price', 'offer-open', 'offer-close', 'volume'];
 
   constructor(private quotesService: QuotesService) { }
 
